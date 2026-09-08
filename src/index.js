eecf40bfe793d8fbd72bb4f71f3313a9abe4f96a
rolando2012 <rolamdomr@gmail.com>
2026-09-08 16:14:39 -0400

    BT-2: Configurar conexión a base de datos (MySQL)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import morgan from "morgan";
+
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
 app.use(morgan("dev"));
 
-app.get('/', (req, res) => res.send("Servidor corriendo"))
+app.get('/', (req, res) => res.send("Servidor corriendo"));
 
 app.listen(PORT, () => {
     console.log(`Server corriendo  en http://localhost:${PORT}`);
