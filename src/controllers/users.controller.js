61ae12a70183ee05cc5581f2a32df82340e0b935
rolando2012 <rolamdomr@gmail.com>
2026-09-09 14:48:55 -0400

    BT-3: Crear usuario

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,3 +1,5 @@
+import { uuidv7 } from "uuidv7";
+import bcrypt from "bcrypt";
 import pool from "../db/connection.js";
 import { validateStore } from "../utils/validations/users.validator.js";
 
@@ -8,5 +10,13 @@ export const store = async (req, res) => {
         return res.status(422).json({ message, errors });
     }
 
-    return res.status(200).json({message: "datos correctos"});
+    const {name, email, password} = req.body;
+
+    const idTexto = uuidv7();
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const query = "INSERT INTO users (id, name, email, password) VALUES (?,?,?,?)";
+    await pool.execute(query, [idTexto, name, email, passwordHash]);
+
+    return res.status(201).json({user: { name: name, email: email }});
 }
