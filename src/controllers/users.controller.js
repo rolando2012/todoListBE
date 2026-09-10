262377f7d5bbb232942ef2d493d7f21436ddfc35
rolando2012 <rolamdomr@gmail.com>
2026-09-10 15:04:52 -0400

    BT-3: Implementar decorador de user y manejo de errores en el controlador

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -2,21 +2,28 @@ import { uuidv7 } from "uuidv7";
 import bcrypt from "bcrypt";
 import pool from "../db/connection.js";
 import { validateStore } from "../utils/validations/users.validator.js";
+import { decoratorUser } from "../decorators/user.decator.js";
 
 export const store = async (req, res) => {
-    const { isValid, message, errors } = validateStore(req.body || {});
+    try {
+        const { isValid, message, errors } = validateStore(req.body || {});
 
-    if(!isValid){
-        return res.status(422).json({ message, errors });
+        if(!isValid){
+            return res.status(422).json({ message, errors });
+        }
+
+        const {name, email, password} = req.body;
+
+        const idTexto = uuidv7();
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        const query = "INSERT INTO users (id, name, email, password) VALUES (?,?,?,?)";
+        await pool.execute(query, [idTexto, name, email, passwordHash]);
+
+        const user = decoratorUser({ id: idTexto, name, email })
+
+        return res.status(201).json({ user });
+    } catch (error) {
+        return res.status(500).json({ message: "Ocurrió un error inesperado en el servidor. Inténtelo más tarde."});
     }
-
-    const {name, email, password} = req.body;
-
-    const idTexto = uuidv7();
-    const passwordHash = await bcrypt.hash(password, 10);
-
-    const query = "INSERT INTO users (id, name, email, password) VALUES (?,?,?,?)";
-    await pool.execute(query, [idTexto, name, email, passwordHash]);
-
-    return res.status(201).json({user: { name: name, email: email }});
 }
